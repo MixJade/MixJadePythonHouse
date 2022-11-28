e5ae096e16ea4e59952564a6fe1575c9168ff3a5
MixJade <1114185977@qq.com>
2022-11-28 22:27:03 +0800

    MixJade

--- 深度学习/绘制MNIST图片.py
+++ 深度学习/绘制MNIST图片.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 # 导入数据集
 mnist = tf.keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-test_images = test_images / 255.0  # 归一化,不然梯度爆炸
+print("训练集样本及标签", train_images.shape, train_labels.shape)
+print("测试集样本及标签", test_images.shape, test_labels.shape)
+train_images, test_images = train_images / 255.0, test_images / 255.0  # 归一化,不然梯度爆炸
 # 进行绘画
 for i in range(15):
     plt.subplot(3, 5, i + 1)
